7bb51151631e904943c762f3c7fdfc1f66bdb6dc
Prekzursil <a_visalon@yahoo.com>
2025-12-06 07:17:15 +0200

    test: provide translate module in app component spec

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,11 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule, AppComponent]
+      imports: [RouterTestingModule, HttpClientTestingModule, TranslateModule.forRoot(), AppComponent]
     }).compileComponents();
   });
 
